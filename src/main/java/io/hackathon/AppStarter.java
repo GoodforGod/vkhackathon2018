632d290e21132b66c1aa52fcee8c1bb66070bdd3
GoodforGod <goodforgod.dev@gmail.com>
2018-11-09 16:43:06 +0300

    init

---
+++ src/main/java/io/hackathon/AppStarter.java
@@ -0,0 +1,13 @@
+package io.hackathon;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppStarter
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
